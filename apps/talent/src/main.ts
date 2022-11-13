3c0dc020e0802eda457854c4130873d6f42e5170
Sérgio Araújo <sergiozaraujoz98@gmail.com>
2022-11-13 18:00:13 +0000

    Moved freelancer to talent

---
+++ apps/talent/src/main.ts
@@ -0,0 +1,8 @@
+import { startLambdaServer } from '@yjcapp/api-utils';
+import { freelancerRouter } from './freelancerRouter';
+import { environment } from './environments/environment';
+
+export const handler = startLambdaServer(
+  freelancerRouter,
+  !environment.production
+);
