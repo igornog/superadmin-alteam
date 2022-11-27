0cf6f90d2cbe57a0785322af89cdc5230fa15fb3
Sérgio Araújo <sergiozaraujoz98@gmail.com>
2022-11-27 12:36:51 +0000

    Remove data base password

--- libs/postgres-db/scripts/exportRemoteDBEnv.sh
+++ libs/postgres-db/scripts/exportRemoteDBEnv.sh
@@ -5,7 +5,7 @@ echo "Exporting database variables for stage $STAGE"
 
 export DB_USERNAME=postgres
 export DB_NAME=$([ "$STAGE" = "prod" ] && echo "alteam_db" || echo "$STAGE")
-export DB_PASSWORD="\$xHA\$17b1t&NyIBGvdZY768CUjCcLNsCb"
+export DB_PASSWORD=$(aws ssm get-parameter --name db_password --with-decryption --output text --query Parameter.Value)
 export DB_ADDRESS=$([ "$STAGE" = "prod" ] && echo "prod.db.alteam.io" || echo "int.db.alteam.io")
 
 echo "DB_USERNAME: $DB_USERNAME"
